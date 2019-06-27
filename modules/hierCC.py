93eb63d7ae9c24b0802b5763b33d4906491d6467
Zhemin Zhou <zhemin.zhou@warwick.ac.uk>
2019-06-27 16:13:29 +0100

    update

--- modules/hierCC.py
+++ modules/hierCC.py
@@ -16,13 +16,64 @@ import os, sys, time, argparse
 import pandas as pd, numpy as np, numba as nb
 from multiprocessing import Pool
 try:
-    from configure import uopen
+    from configure import uopen, logger, xrange
 except :
-    from .configure import uopen
-try:
-    xrange(3)
-except :
-    xrange = range
+    from .configure import uopen, logger, xrange
+
+
+@nb.jit(nopython=True)
+def assignment2(dists, res) :
+    for id in xrange(len(dists)) :
+        idx, ref, jd = dists[id]
+        res[idx, jd:] = res[ref, jd:]
+
+def get_distance2(idx) :
+    global mat
+    n_loci = mat.shape[1] - 1
+    if idx == 0 or idx >= mat.shape[0] :
+        return np.zeros(shape=[0, 3], dtype=int)
+    profile = mat[idx]
+    s = np.sum((profile[1:] == mat[:idx, 1:]) & (profile[1:] > 0), 1)
+    ql = np.sum(profile[1:] > 0)
+    rl = np.sum(mat[:idx, 1:] > 0, 1)
+    rll = n_loci - np.max([(n_loci - ql)*3, int((n_loci - ql)+n_loci*0.03+0.5)])
+    rl[rl < rll] = rll
+    rl[rl > ql ] = ql
+    d = ((rl - s).astype(float)*n_loci/rl+0.5).astype(int)+1
+    dists = np.vstack([np.repeat(idx, idx), np.arange(idx), d]).astype(int).T
+    return dists[np.argmin(dists.T[2])]
+
+#@nb.jit(nopython=True)
+def assignment3(dists, res, encode, presence) :
+    n_loci = mat.shape[1] - 1
+    for id in xrange(len(dists)) :
+        idx, ref, _, s, ql = dists[id]
+        gl = presence[encode[res[ref, 1:]], np.arange(1, res.shape[1])]
+        gl[gl > ql] = ql
+        d = (n_loci*(gl-s).astype(float)/gl+0.5).astype(int)
+        jd = np.argmax( (d - np.arange(n_loci)) <= 0 ) + 1
+        res[idx, jd:] = res[ref, jd:]
+        gl = presence[encode[res[idx, jd:]], np.arange(jd, presence.shape[1])]
+        presence[encode[res[idx, jd:]][gl < ql], np.arange(jd, presence.shape[1])[gl < ql]] = ql
+        
+        
+
+def get_distance3(idx) :
+    global mat
+    n_loci = mat.shape[1] - 1
+    if idx == 0 or idx >= mat.shape[0] :
+        return np.zeros(shape=[0, 3], dtype=int)
+    profile = mat[idx]
+    s = np.sum((profile[1:] == mat[:idx, 1:]) & (profile[1:] > 0), 1)
+    ql = np.sum(profile[1:] > 0)
+    rl = np.sum(mat[:idx, 1:] > 0, 1)
+    rll = n_loci - np.max([(n_loci - ql)*3, int((n_loci - ql)+n_loci*0.03+0.5)])
+    rl[rl < rll] = rll
+    rl[rl > ql ] = ql
+    d = ((rl - s).astype(float)*n_loci/rl+0.5).astype(int)
+    dists = np.vstack([np.repeat(idx, idx), np.arange(idx), d, s, np.repeat(ql, idx)]).astype(int).T
+    return dists[np.argmin(dists.T[2])]
+
 
 def get_distance(idx) :
     global mat
@@ -30,10 +81,11 @@ def get_distance(idx) :
     if idx == 0 or idx >= mat.shape[0] :
         return np.zeros(shape=[0, 4], dtype=int)
     profile = mat[idx]
-    d1 = (n_loci * np.sum((profile[1:] != mat[:idx, 1:]) & (profile[1:] > 0), 1)/np.sum(profile[1:] > 0).astype(float)+0.5).astype(int)+1
+    ql = np.max(1.0, np.sum(profile[1:] > 0).astype(float))
+    d1 = (n_loci * np.sum((profile[1:] != mat[:idx, 1:]) & (profile[1:] > 0), 1)/ql+0.5).astype(int)+1
     d2 = n_loci - np.sum((profile[1:] == mat[:idx, 1:]) & (profile[1:] > 0), 1)+1
     d1[d1>d2] = d2[d1>d2]
-    dists = np.vstack([[idx for i in np.arange(idx)], np.arange(idx), d1, d2]).astype(int).T
+    dists = np.vstack([np.repeat(idx, idx), np.arange(idx), d1, d2]).astype(int).T
     return dists[np.argsort(dists.T[2], kind='mergesort')]
 
 @nb.jit(nopython=True)
@@ -76,7 +128,7 @@ work out specialised single linkage clustering result of all the profiles in the
 def hierCC(args) :
     params = get_args(args)
     ot = time.time()
-    profile_file, cluster_file, old_cluster = params.profile, params.output+'.npy', params.incremental
+    profile_file, cluster_file, old_cluster = params.profile, params.output+'.npz', params.incremental
     
     global mat, n_loci
     mat = pd.read_csv(profile_file, sep='\t', header=None, dtype=str).values
@@ -84,47 +136,70 @@ def hierCC(args) :
     mat = mat[1:, allele_columns].astype(int)
     n_loci = mat.shape[1] - 1
     
-    sys.stderr.write('{0}: Loaded in allelic profiles with dimension: {1} and {2}. The first column is assumed to be type id. \n'.format(time.time() - ot, *mat.shape))
+    logger('{0}: Loaded in allelic profiles with dimension: {1} and {2}. The first column is assumed to be type id.'.format(time.time() - ot, *mat.shape))
     if not params.immutable :
         absence = np.sum(mat <= 0, 1)
         mat = mat[np.argsort(absence, kind='mergesort')]
 
-    res = np.zeros(shape=mat.shape, dtype=int) #np.memmap(cluster_file, shape=mat.shape, dtype=int, mode='w+')
-    res.T[0] = n_loci+1
-    res[0] = mat[0, 0]
+    if params.immutable :
+        presence = np.repeat(np.sum(mat[:, 1:]>0, 1), mat.shape[1]).reshape(mat.shape)
+        presence.T[0] = mat.T[0]
+        encode = np.zeros(np.max(mat.T[0])+1, dtype=int)
+        encode[mat.T[0]] = np.arange(mat.shape[0])
+    else :
+        presence = None
     if os.path.isfile(old_cluster) :
-        cls = np.load(old_cluster)
-        #n_row = cls.size / mat.shape[1]
-        #cls.shape = (n_row, mat.shape[1])
+        od = np.load(old_cluster)
+        cls = od['hierCC']
+        if params.immutable :
+            if 'presence' in od :
+                old_presence = od['presence'] 
+                if old_presence is not None and old_presence.size > 0 :
+                    for c in old_presence :
+                        if c[0] > 0 :
+                            presence[encode[c[0]], :] = c
+                else :
+                    old_presence = None
+            else :
+                old_presence = None
+
         typed = {c[0]:id for id, c in enumerate(cls) if c[0] > 0}
         if len(typed) > 0 :
-            sys.stderr.write('{0}: Loaded in {1} old hierCC assignments. \n'.format(time.time()-ot, len(typed)))
+            logger('{0}: Loaded in {1} old hierCC assignments.'.format(time.time()-ot, len(typed)))
             mat_idx = np.array([t in typed for t in mat.T[0]])
             mat[:] = np.vstack([mat[mat_idx], mat[(mat_idx) == False] ])
     else :
         typed = {}
-    
-    sys.stderr.write('{0}: Start hierCC assignments\n'.format(time.time()-ot))
+
+    logger('{0}: Start hierCC assignments'.format(time.time()-ot))
     pool = Pool(10)
+    
+    res = np.repeat(mat.T[0], mat.shape[1]).reshape(mat.shape)
+    res[1:, 0] = n_loci+1
     for index in xrange(0, mat.shape[0], 100) :
         to_run = []
         for idx in np.arange(index, index+100) :
             if idx < mat.shape[0] :
                 if mat[idx, 0] in typed :
                     res[idx, :] = cls[typed[mat[idx, 0]], :]
+                    if params.immutable and old_presence is None :
+                        gl = presence[encode[res[idx, 1:]], np.arange(1, presence.shape[1])]
+                        ql = np.sum(mat[idx, 1:] > 0)
+                        presence[encode[res[idx, 1:]][gl < ql], np.arange(1, presence.shape[1])[gl < ql]] = ql
                 else :
-                    res[idx, 1:] = mat[idx, 0]
                     to_run.append(idx)
         if len(to_run) == 0 :
             continue
         if not params.immutable :
             dists = np.vstack( pool.map(get_distance, to_run) )
+            assignment(dists, res)
         else :
-            dists = np.vstack( [d[0] for d in pool.map(get_distance, to_run) if len(d)] )
-        assignment(dists, res)
-        sys.stderr.write('{0}: Assigned {1} of {2} types into hierCC.\n'.format(time.time() - ot, index, mat.shape[0]))
+            dists = np.vstack( pool.map(get_distance3, to_run) )
+            assignment3(dists, res, encode, presence)
+
+        logger('{0}: Assigned {1} of {2} types into hierCC.'.format(time.time() - ot, index, mat.shape[0]))
     res.T[0] = mat.T[0]
-    np.save(cluster_file, res)
+    np.savez_compressed(cluster_file, hierCC=res, presence=presence)
 
     if not params.delta :
         with uopen(params.output+'.hierCC.gz', 'w') as fout :
@@ -138,8 +213,8 @@ def hierCC(args) :
             for r in res[np.argsort(res.T[0])] :
                 fout.write('\t'.join([str(r[id+1]) for id in deltas])+'\n')
     del res
-    sys.stderr.write('NUMPY clustering result (for incremental hierCC): {0}.npy\n'.format(params.output))
-    sys.stderr.write('TEXT  clustering result (for visual inspection): {0}.hierCC.gz\n'.format(params.output))
+    logger('NUMPY clustering result (for incremental hierCC): {0}.npz'.format(params.output))
+    logger('TEXT  clustering result (for visual inspection): {0}.hierCC.gz'.format(params.output))
 
 
 mat, n_loci = None, None
